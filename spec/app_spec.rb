d9baa8efb70723a4d94d53226436efcce1ab10cf
guillaume <gueyome@hotmail.fr>
2019-10-09 16:29:52 +0200

    like_follow

---
+++ spec/app_spec.rb
@@ -0,0 +1,10 @@
+require_relative '../lib/app'
+
+describe "the login_twitter method" do
+    it "should return client, and client is not nil" do
+      expect(login_twitter).not_to be_nil
+    end
+end
+
+#describe "it sends 5 tweets to random users"
+#    it ""
